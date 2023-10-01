d6f693558a81388d0716e03f866451e6d0ea05e6
felipechak <felipepacin7@gmail.com>
2023-10-01 02:49:59 -0300

    Removed background music and made style adjustments

--- main.js
+++ main.js
@@ -1,34 +1,38 @@
 document.addEventListener("DOMContentLoaded",()=>{
-    let bgaudio = document.getElementById("bgaudio")
-    bgaudio.volume = 0.25
-    const utterThis = new SpeechSynthesisUtterance()
-    let finishedQuote = false
+    let pageBody = document.getElementsByTagName('body')[0]
     let quoteh4 = document.getElementById("quote")
+    let author = document.getElementById("author")
+    
     const getNewQuote = async () => {
-        let apiResponse = await fetch('https://api.kanye.rest/')
+        let apiResponse = await fetch('https://api.quotable.io/random')
         return await apiResponse.json()
     }
     const refreshQuote = () => {
         getNewQuote().then((response)=>{
-            console.log(response.quote)
+            console.log(response.content)
             quoteh4.style.opacity = 0;
+            author.style.opacity = 0;
             setTimeout(function(){
-                quoteh4.innerText = response.quote
+                quoteh4.innerText = response.content
+                author.innerText = response.author
                 quoteh4.style.opacity = 1;
-                const synth = window.speechSynthesis
-                console.log(synth.getVoices())
-                utterThis.text = response.quote
-                utterThis.pitch = 1.2;
-                utterThis.rate = 1.3;
-
-                utterThis.onend = function() {
-                    console.log("finish")
-                    refreshQuote();
-                };
-                synth.speak(utterThis);
+                author.style.opacity = 1;
             },500)
+            setTimeout(()=>{
+                refreshQuote();
+            },4000)
         })
     }
+    const bgFiles = ['bg.jpg' ,'bg2.png','bg3.jpg','bg4.jpg','bg5.jpg','bg6.jpg']
+    setInterval(()=>{
+        let newBgImageName =  bgFiles[Math.floor(Math.random()*bgFiles.length)] 
+        var image = new Image();
+          // Image for transition
+        image.src = `img/${newBgImageName}`;
+        image.onload = function () {
+            pageBody.style.backgroundImage = `url(${image.src})`
+        };
+    },3500)
     refreshQuote()
     /* while(true){
         getNewQuote().then((response)=>{
